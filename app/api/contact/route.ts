0f22d1d0a3ea0cacb92cd48add5a341722973146
jacevesl <jacevesl@gmail.com>
2023-07-16 16:08:33 -0600

    Contact form connected to db(local)

---
+++ app/api/contact/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import fs from 'fs'
+import path from 'path'
+// Suggestion by the linter instead  of
+// const fs =require('fs')
+// const path =require('path')
+export async function POST(request: NextRequest){
+    
+        const data = await request.json()
+    // Read DB (JSON file)
+        const filePath = path.resolve(process.cwd(),'app/data/submission.json')
+    // The process.cwd() method returns the current working directory of the Node.js process.
+        let submission:any=[]
+        try {
+            const data = fs.readFileSync(filePath, 'utf8')
+            submission = JSON.parse(data)
+        } catch (error) {
+            console.log('Error reading file', error)
+        }
+    // Here we add the new data and write it back
+        submission.push(data)
+        try {
+            const newData =JSON.stringify(submission, null, 2)
+            fs.writeFileSync(filePath,newData, 'utf8')
+        } catch (error) {
+            console.log('Error writing file', error)
+        }
+        return NextResponse.json({
+            data:data,
+            message: 'This message has been successfully sent'
+        })
+    }
+
+// export async function GET(){
+//     return NextResponse.json({
+//         message: 'Whatever'
+//     })
+// }
--- app/contact-me/components/form.tsx
+++ app/contact-me/components/form.tsx
@@ -1,60 +1,91 @@
 'use client'
-import React from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
-  return (
-    <form 
-    onSubmit={() => console.log(s)} 
-    className='bg-white '
-    >
+    const [isMessageSent, setMessageSent] = useState<boolean>(false);
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const target = event.currentTarget;
+
+        const name = target.elements.namedItem("name") as HTMLInputElement;
+        const company = target.elements.namedItem("company") as HTMLInputElement;
+        const email = target.elements.namedItem("email") as HTMLInputElement;
+        const message = target.elements.namedItem("message") as HTMLInputElement;
+
+        const data = {
+            name: name.value,
+            company: company.value,
+            email: email.value,
+            message: message.value,
+        };
+        try {
+            const response = await fetch('/api/contact', 
+            {   method:'POST',
+            body: JSON.stringify(data),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            if(!response.ok){throw new Error('HTTP error status:' + response.status)}
+            setMessageSent(true);
+        } catch (error:any) {
+            console.log('There was a server error' + error.message)
+        }
+    }
+    return (
+        <>
+    
+    
+
+    <form onSubmit={handleSubmit} className='bg-white'>
         <div className='mb-4'>
-            <label 
-            className='label-form'
-            htmlFor='name'
-            >Name</label>
+            <label className='label-form' htmlFor='name'>Name</label>
             <input
             id='name'
             type='text'
             name='name'
             className='input-form'
+            required
+            minLength={3}
+            maxLength={200}
             />
         </div>
           <div className='mb-4'>
-              <label
-                  className='label-form'
-                  htmlFor='email'
-              >Email</label>
+              <label className='label-form' htmlFor='email'>Email</label>
               <input
                   id='email'
                   type='text'
                   name='email'
                   className='input-form'
+                  required
+                  minLength={5}
+                  maxLength={200}
               />
           </div>
           <div className='mb-4'>
-              <label
-                  className='label-form'
-                  htmlFor='company'
-            //       The htmlFor property sets or returns the value of the for attribute of a label.
-            //       The for attribute specifies which form element a label is bound to.
-              >Company</label>
+              <label className='label-form' htmlFor='company'>Company</label>
+            {/* The htmlFor property sets or returns the value of the for attribute of a label.
+                The for attribute specifies which form element a label is bound to. */}
+ 
               <input
-                  id=  'company'
+                  id= 'company'
                   type='text'
                   name='company'
                   className='input-form'
+                  minLength={2}
+                  maxLength={200}
               />
           </div>
           <div className='mb-4'>
-              <label
-                  className='label-form'
-                  htmlFor='message'
-              >Message</label>
+              <label className='label-form' htmlFor='message'>Message</label>
               <textarea
                   id='message'
                 //   type='text'
                   name='message'
                   className='input-form'
+                  required
+                  minLength={10}
+                  maxLength={1000}
               />
           </div>
           <button 
@@ -68,6 +99,8 @@ const Form = () => {
           hover:text-blue hover:bg-white hover:border'
           type='submit'>Send Message</button>
     </form>
+            {isMessageSent && <p> Your message has been sent, thank you</p>}
+        </>
   )
 }
 
